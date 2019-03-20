1b76673cae93a555a37d746b0f8cd7d79e657134
lokada <lyokada@ucsc.edu>
2019-03-20 14:16:50 -0700

    win state clean up

--- Assets/Scripts/WinStateScript.cs
+++ Assets/Scripts/WinStateScript.cs
@@ -13,22 +13,27 @@ public class WinStateScript : MonoBehaviour
 
     void OnTriggerEnter(Collider col)
     {
-        if (col.gameObject.tag == "Player1" && string.Compare(winner,"") == 0)
+        if (col.gameObject.GetComponent<PlayerScript>().pollenAmount >= 38.0f)
         {
-            p1WinStateText.text = "Winner!!!";
-            p1WinStateText.gameObject.SetActive(true);
-        } else if (col.gameObject.tag == "Player1" && string.Compare(winner,"") != 0) {
-            p1WinStateText.text = "Loser...";
-            p1WinStateText.gameObject.SetActive(true);
-        }
+            if (col.gameObject.tag == "Player1" && string.Compare(winner,"") == 0)
+            {
+                p1WinStateText.text = "Winner!!!";
+                p1WinStateText.gameObject.SetActive(true);
 
-        if (col.gameObject.tag == "Player2" && string.Compare(winner,"") == 0)
-        {
-            p2WinStateText.text = "Winner!!!";
-            p2WinStateText.gameObject.SetActive(true);
-        } else if (col.gameObject.tag == "Player2" && string.Compare(winner,"") != 0){
-            p2WinStateText.text = "Loser...";
-            p2WinStateText.gameObject.SetActive(true);
-        }
+                p2WinStateText.text = "Loser...";
+                p2WinStateText.gameObject.SetActive(true);
+            } else if (col.gameObject.tag == "Player1" && string.Compare(winner,"") != 0) {
+                
+            }
+
+            if (col.gameObject.tag == "Player2" && string.Compare(winner,"") == 0)
+            {
+                p2WinStateText.text = "Winner!!!";
+                p2WinStateText.gameObject.SetActive(true);
+
+                p1WinStateText.text = "Loser...";
+                p1WinStateText.gameObject.SetActive(true);
+            }
+        }   
     }
 }
